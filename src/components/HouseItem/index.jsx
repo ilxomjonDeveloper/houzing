e9955a7a42b7651791e90cd1f279bec80a1b40db
Ilxomjon <ilxomjondeveloper@gmail.com>
2023-03-28 16:53:56 +0500

    house item delete changes

--- src/components/HouseItem/index.jsx
+++ src/components/HouseItem/index.jsx
@@ -1,16 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React from "react";
 import { Container } from "./style";
-const { REACT_APP_BASE_URL: url } = process.env;
+// const { REACT_APP_BASE_URL: url } = process.env;
 
 const HouseItem = () => {
-  const [data, setData] = useState({});
-  const params = useParams();
-  useEffect(() => {
-    params?.id && fetch(`${url}/houses/list/${params?.id}`)
-      .then((res) => res.json())
-      .then((res) => setData(res?.data || []));
-  }, [params?.id]);
+  // const [data, setData] = useState({});
+  // const params = useParams();
+  // useEffect(() => {
+  //   params?.id && fetch(`${url}/houses/list/${params?.id}`)
+  //     .then((res) => res.json())
+  //     .then((res) => setData(res?.data || []));
+  // }, [params?.id]);
   return (
     <Container>
       <h1>House Item</h1>
